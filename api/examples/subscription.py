1de066e3b2aefb704be8f157e118c9839c280113
Dawid Liberda <dave.liberda@yahoo.com>
2023-01-10 00:25:48 +0100

    Added example usage of APICopernicus with subscription

---
+++ api/examples/subscription.py
@@ -0,0 +1,26 @@
+from utils.copernicus_observer import CopernicusObserver
+from api_copernicus import APICopernicus
+from utils.event_type import EventType
+from time import sleep
+
+
+class App(CopernicusObserver):
+    def __init__(self):
+        self.api = APICopernicus()
+
+        self.api.subscribe([EventType.BUTTON1], self)
+
+    def update(self, event_type: EventType, event_value: int) -> None:
+        print(f"Getting event type {event_type} with value {event_value} directly in the app")
+
+
+app = App()
+
+led_state = True
+
+while True:
+    print(f"Setting LED1 state to {led_state}")
+    app.api.set_led1_state(led_state)
+
+    sleep(0.5)
+    led_state = not led_state
